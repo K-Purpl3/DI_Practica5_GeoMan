ef6513a02845a5c5fc0087815838bdf02ef881a3
purpl3 <mr.georgemanea@gmail.com>
2025-10-22 15:48:59 +0200

    Ej2_Practica-5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,9 @@ android {
 
 dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
